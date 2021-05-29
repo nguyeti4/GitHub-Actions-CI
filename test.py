49b8f602765d575ebb336f1b70a3045f5a8f0821
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-29 16:34:03 -0700

    added tests for multiply and divide

--- test.py
+++ test.py
@@ -9,3 +9,9 @@ class TestCase(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(example.subtract(1, 1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(example.multiply(3, 4), 12)
+
+    def test_divide(self):
+        self.assertEqual(example.divide(12, 4), 3)
